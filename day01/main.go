5550a9d14089c2ec8cdc441befb1c185c8e5933c
clavesi <34937562+clavesi@users.noreply.github.com>
2024-12-03 11:23:06 -0600

    day01 part 2 done

--- day01/main.go
+++ day01/main.go
@@ -73,4 +73,38 @@ func part1(input string) int {
 	return total
 }
 
-func part2(input string) int { return 0 }
+func part2(input string) int {
+	// Construct two hash maps
+	// Both maps have the key=number and value=# of times that showed up
+	left := make(map[int]int)
+	right := make(map[int]int)
+	f, err := os.Open(input)
+	utils.Check(err)
+	defer f.Close()
+
+	// Go line by line
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		result := strings.Split(scanner.Text(), "   ")
+		leftVal, err := strconv.Atoi(result[0])
+		rightVal, err := strconv.Atoi(result[1])
+		if err != nil {
+			panic(err)
+		}
+		left[leftVal] = left[leftVal] + 1
+		right[rightVal] = right[rightVal] + 1
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	// Then iterate over left map's k-v and get value from right map
+	// Then it's (left_key * left_index) ^ right_val
+	total := 0
+	for idx, val := range left {
+		total += (idx * val) * right[idx]
+	}
+
+	return total
+}
